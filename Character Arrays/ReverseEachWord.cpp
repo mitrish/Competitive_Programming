42b98db09facd320eef5c9040b59b1a932b08653
Rishabh <rishabhmittal200@gmail.com>
2019-06-04 22:58:26 +0530

    Added ReverseEachWord

--- Character Arrays/ReverseEachWord.cpp
+++ Character Arrays/ReverseEachWord.cpp
@@ -2,17 +2,28 @@
     #include<bits/stdc++.h>
     using namespace std;
 
-    void ReverseEachString(string str){
-        string output,temp;
-        for(int i = str.length(); i > 0; i--){
-            if(str[i] == "\t"){
-                output += str[i]
-            }
-            temp = str[i];
-            output = temp + output;
+    void reverse(string str, int start, int end){
+        while(start < end){
+            char temp = str[start];
+            str[start] = str[end];
+            start++;
+            end--; 
         }
+    }
+    void ReverseEachWord(string str){
+        // Input string - Rishabh Mittal
+        // Output string - hbahsiR lattiM
+        int start = 0, end = 0;
+        for(int i = 0; str[i] != 0; i++){
+            if(input[i] == ' '){
+                end = i - 1;
+                reverse(str, start, end);
+                start = i + 1;
+            }
+        }
+        end = i - 1;
+        reverse(str, start, end);
 
-        cout << output << endl;
     }
 
     int main(){
