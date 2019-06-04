253803256382d9fd025369925b146eb29d9a2276
Rishabh <rishabhmittal200@gmail.com>
2019-06-05 00:22:48 +0530

    added compress string

--- Character Arrays/CompressString.cpp
+++ Character Arrays/CompressString.cpp
@@ -1,11 +1,26 @@
 #include<iostream>
 #include<bits/stdc++.h>
-using namespace stat
+using namespace std;
 
-void compress string(){
-    
+void CompressString(string str){
+    string temp = str[0];
+    string output;
+    int count = 1;
+    for(int i =0;i<str.length();i++){
+        if(str[i] == temp){
+            count++;
+        }else{
+            output = str[i] + count;
+            temp = str[i];
+            count = 1;
+        }
+    }
+
+    cout << output;
 }
 
 int main(){
-
+    string input;
+    std::getline(std::cin, input);
+    CompressString(input);
 }
